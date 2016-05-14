7a270d30518584dfde0edb34bec9a91ff0b05fcf
James P. Howard, II <jh@jameshoward.us>
2016-05-14 11:32:02 -0400

    Add the tests for Phonex

---
+++ tests/testthat/test-phonex.R
@@ -0,0 +1,9 @@
+context("phonex")
+
+##  Test the Phonex algorithm
+test <- read.csv("phonex.csv", comment.char="#", stringsAsFactors = FALSE, colClasses = c("character", "character"), encoding = "UTF-8")
+for(i in 1:nrow(test))
+    expect_true(phonex(test$word[i]) == test$value[i])
+test$test <- phonex(test$word)
+for(i in 1:nrow(test))
+    expect_true(test$test[i] == test$value[i])
