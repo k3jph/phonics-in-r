cd912c381958a946e6963d1bb6623ac7bd41030c
James P. Howard, II <jh@jameshoward.us>
2019-02-28 19:47:11 -0500

    Added new phonics command

--- R/phonics.R
+++ R/phonics.R
@@ -25,24 +25,104 @@
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #' @title Phonetic Spelling Algorithms
-#' @docType package
 #'
-#' @description
-#' Encode words phonetic spelling algorithms.
+#' @description 
 #'
-#' @details
 #' The \code{phonics} package for \code{R} is designed to provide a
 #' variety of phonetic indexing algorithms in common and not-so-common
 #' use today.  The algorithms generally reduce a string to a symbolic
 #' representation approximating the sound made by pronouncing the
-#' string.  They can be used to match names, words, and as a proxy for
+#' string.  They can be used to match names, strings, and as a proxy for
 #' assorted string distance algorithms.  The algorithm reduces a string
 #' to a symbolic representation approximating the sound.  It can be used
-#' to match names, words, and as a proxy for assorted string distance
+#' to match names, strings, and as a proxy for assorted string distance
 #' algorithms.
 #'
-#' @author
-#' James P. Howard, II <jh@jameshoward.us>
+#' @param word string or vector of strings to encode
+#' @param method vector of method names to use
+#' @param clean if \code{TRUE}, return \code{NA} for unknown alphabetical characters
 #'
-"_PACKAGE"
-#> [1] "_PACKAGE"
+#' @details
+#'
+#' The \code{phonics} package for \code{R} is designed to provide a
+#' variety of phonetic indexing algorithms in common and not-so-common
+#' use today.  The algorithms generally reduce a string to a symbolic
+#' representation approximating the sound made by pronouncing the
+#' string.  They can be used to match names, strings, and as a proxy for
+#' assorted string distance algorithms.  The algorithm reduces a string
+#' to a symbolic representation approximating the sound.  It can be used
+#' to match names, strings, and as a proxy for assorted string distance
+#' algorithms.
+#' 
+#' The variable \code{word} is the name to be encoded.  The variable
+#' \code{maxCodeLen} is \emph{not} supported in this algorithm encoder
+#' because the algorithm itself is dependent upon its six-character
+#' length.  The variables \code{x} and \code{y} are MRA-encoded and are
+#' compared to each other using the MRA comparison specification.
+#'
+#' Different phonetic algorithm are only defined for inputs over the
+#' limited alphabets, Non-alphabetical characters are removed from the
+#' string in a locale-dependent fashion.  This strips spaces, hyphens,
+#' and numbers.  For inputs outside of its known range, the output is
+#' undefined and \code{NA} is returned and a \code{warning} this thrown.
+#' If \code{clean} is \code{FALSE}, \code{phonics} attempts to process
+#' the strings.  The default is \code{TRUE}.
+#'
+#' @return
+#'
+#' Returns a data frame containing the phonetic spellings of the input
+#' for each method applied.
+#'
+#' @family phonics
+#'
+#' @examples
+#' phonics(c("Peter", "Peady"), c("soundex", "soundex.refined"))
+#'
+#' @importFrom data.table :=
+#' @importFrom data.table data.table
+#'
+#' @export
+phonics <- function(word, method, clean = TRUE) {
+    ret <- data.table(word = word)
+
+    for(i in method) {
+        if("caverphone" %in% i)
+            ret[, "caverphone" := caverphone(word, clean = clean)]
+        else if("caverphone.modified" %in% i)
+            ret[, "caverphone.modified" := caverphone(word, modified = TRUE, clean = clean)]
+        else if("cologne" %in% i)
+            ret[, "cologne" := cologne(word, clean = clean)]
+        else if("lein" %in% i)
+            ret[, "lein" := lein(word, clean = clean)]
+        else if("metaphone" %in% i)
+            ret[, "metaphone" := metaphone(word, clean = clean)]
+        else if("nysiis" %in% i)
+            ret[, "nysiis" := nysiis(word, clean = clean)]
+        else if("nysiis.modified" %in% i)
+            ret[, "nysiis.modified" := nysiis(word, modified = TRUE, clean = clean)]
+        else if("onca" %in% i)
+            ret[, "onca" := onca(word, clean = clean)]
+        else if("onca.modified" %in% i)
+            ret[, "onca.modified" := onca(word, modified = TRUE, clean = clean)]
+        else if("onca.refined" %in% i)
+            ret[, "onca.refined" := onca(word, refined = TRUE, clean = clean)]
+        else if("onca.modified.refined" %in% i)
+            ret[, "onca.modified.refined" := onca(word, modified = TRUE, refined = TRUE, clean = clean)]
+        else if("phonex" %in% i)
+            ret[, "phonex" := phonex(word, clean = clean)]
+        else if("rogerroot" %in% i)
+            ret[, "rogerroot" := rogerroot(word, clean = clean)]
+        else if("soundex" %in% i)
+            ret[, "soundex" := soundex(word, clean = clean)]
+        else if("soundex.refined" %in% i)
+            ret[, "soundex.refined" := refinedSoundex(word, clean = clean)]
+        else if("statcan" %in% i)
+            ret[, "statcan" := statcan(word, clean = clean)]
+        else
+            warning(paste("unknown phonetic spelling algorithm:", i))
+    }
+
+    ## Clean it up and make it pretty
+    ret <- as.data.frame(ret)
+    return(ret)
+}
